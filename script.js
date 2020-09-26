afb9f853e5a0f5d47c5c22d52cb522ad351f9bed
dmfuchs <dm.fuchs00@gmail.com>
2020-09-26 14:49:10 +0200

    Modded

--- script.js
+++ script.js
@@ -2,14 +2,21 @@ var y = Math.floor(Math.random() * 10 + 1);
 
 var guess = 1
 
+
+document.getElementById("resetForm").onclick = function clear1 (){
+    document.getElementById("guessField").value = ""
+
+}
+
+
 document.getElementById("submitGuess").onclick  = function (){
 
     var x = document.getElementById("guessField").value
 
     if (x == y)
     {
-        alert("CONGRATULATIONS!!!!! YOU GUESSED IT RIGH IN " + guess + " GUESSEES ");
-
+        alert("CONGRATULATIONS!!!!! YOU GUESSED IT RIGH IN " + guess + " GUESSEES!! ");
+        document.getElementById("guessField").value = ""
 
     }
     else if (x > y)
@@ -17,12 +24,12 @@ document.getElementById("submitGuess").onclick  = function (){
     {
         guess++;
         alert("OOPS!! THE NUMBER YOU GUESSED IS TOO HIGH, TRY A SMALLER NUMBER")
-
+        document.getElementById("guessField").value = ""
     }
     else 
     {
         guess++;
         alert("OOPS!! THE NUMBER YOU GUESSED IS TOO LOW, TRY A GREATER NUMBER") 
-
+        document.getElementById("guessField").value = ""
     }
 }
